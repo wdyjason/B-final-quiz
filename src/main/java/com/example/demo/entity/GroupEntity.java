850df8a56afa67359827ae4d019f4cd011dbec96
wdyjason <ddd123321456555@qq.com>
2020-09-24 11:23:24 +0800

    init basic project structure

---
+++ src/main/java/com/example/demo/entity/GroupEntity.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class GroupEntity {
+}
